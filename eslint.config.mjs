81133906a95d759db53909831e0e17499de9d312
Adam Lui <adam@kudoai.com>
2025-04-27 05:46:25 -0700

    Added `styles` to globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,8 +21,8 @@ export default [
                 chatgpt: 'readonly', chrome: 'readonly', config: 'writable', CryptoJS: 'readonly',
                 cryptoUtils: 'readonly', deepseek: 'readonly', dom: 'readonly', GM_cookie: 'readonly', hljs: 'readonly',
                 icons: 'writable', importScripts: 'readonly', JSON5: 'readonly', ipv4: 'readonly', marked: 'readonly',
-                modals: 'writable', renderMathInElement: 'readonly', settings: 'writable', toggles: 'writable',
-                tooltip: 'writable', ui: 'readonly'
+                modals: 'writable', renderMathInElement: 'readonly', settings: 'writable', styles: 'readonly',
+                toggles: 'writable', tooltip: 'writable', ui: 'readonly'
             }
         },
         plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
