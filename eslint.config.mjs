740dcccd6868f66ddb80c9bf7c35df726be8e94e
Adam Lui <adam@kudoai.com>
2025-05-09 06:55:44 -0700

    Added `infinity` to globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,11 +20,11 @@ export default [
                 ...globals.browser, ...globals.greasemonkey, ...globals.node, app: 'writable', buttons: 'writable',
                 chatbar: 'writable', chatgpt: 'readonly', chrome: 'readonly', config: 'writable', CryptoJS: 'readonly',
                 cryptoUtils: 'readonly', deepseek: 'readonly', dom: 'readonly', env: 'writable', GM_cookie: 'readonly',
-                hljs: 'readonly', icons: 'writable', importScripts: 'readonly', JSON5: 'readonly', ipv4: 'readonly',
-                marked: 'readonly', modals: 'writable', notify: 'readonly', renderMathInElement: 'readonly',
-                settings: 'writable', sites: 'writable', styles: 'readonly', syncConfigToUI: 'readonly',
-                toggleMode: 'readonly', toggles: 'writable', tooltip: 'writable', ui: 'readonly',
-                updateCheck: 'readonly'
+                hljs: 'readonly', icons: 'writable', importScripts: 'readonly', infinity: 'readonly', JSON5: 'readonly',
+                ipv4: 'readonly', marked: 'readonly', modals: 'writable', notify: 'readonly',
+                renderMathInElement: 'readonly', settings: 'writable', sites: 'writable', styles: 'readonly',
+                syncConfigToUI: 'readonly', toggleMode: 'readonly', toggles: 'writable', tooltip: 'writable',
+                ui: 'readonly', updateCheck: 'readonly'
             }
         },
         plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
