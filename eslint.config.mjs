a2833e51350e9d814ab9d1b480352f8c100a2383
Adam Lui <adam@kudoai.com>
2025-05-13 22:59:18 -0700

    Abstracted sync methods to lib/sync.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,13 +18,14 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.greasemonkey, ...globals.node, app: 'writable', browserAPI: 'readonly',
-                buttons: 'writable', chatbar: 'writable', chatgpt: 'readonly', chrome: 'readonly', config: 'writable',
-                CryptoJS: 'readonly', cryptoUtils: 'readonly', deepseek: 'readonly', dom: 'readonly', env: 'writable',
-                GM_cookie: 'readonly', hljs: 'readonly', icons: 'writable', importScripts: 'readonly',
-                infinity: 'readonly', JSON5: 'readonly', ipv4: 'readonly', marked: 'readonly', modals: 'writable',
-                notify: 'readonly', renderMathInElement: 'readonly', settings: 'writable', sites: 'writable',
-                styles: 'readonly', sync: 'readonly', syncConfigToUI: 'readonly', toggleMode: 'readonly',
-                toggles: 'writable', tooltip: 'writable', ui: 'readonly', updateCheck: 'readonly'
+                buttons: 'writable', chatbar: 'writable', chatgpt: 'readonly', checkBtnsToClick: 'readonly',
+                chrome: 'readonly', config: 'writable', CryptoJS: 'readonly', cryptoUtils: 'readonly',
+                deepseek: 'readonly', dom: 'readonly', env: 'writable', GM_cookie: 'readonly', hljs: 'readonly',
+                icons: 'writable', importScripts: 'readonly', infinity: 'readonly', JSON5: 'readonly', ipv4: 'readonly',
+                marked: 'readonly', modals: 'writable', notify: 'readonly', renderMathInElement: 'readonly',
+                settings: 'writable', sites: 'writable', styles: 'readonly', sync: 'readonly',
+                syncConfigToUI: 'readonly', toggleMode: 'readonly', toggles: 'writable', toolbarMenu: 'writable',
+                tooltip: 'writable', ui: 'readonly', updateCheck: 'readonly'
             }
         },
         plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
