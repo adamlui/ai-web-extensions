821f9966bbcb7c8a2113083c94b1715c97efbe0b
Adam Lui <adam@kudoai.com>
2025-05-31 06:51:22 -0700

    Bumped dom.js to dom.min.js

--- deepseek-omnibox/firefox/extension/content.js
+++ deepseek-omnibox/firefox/extension/content.js
@@ -1,5 +1,5 @@
 (async () => {
-    for (const resource of ['lib/deepseek.js', 'lib/dom.js']) await import(chrome.runtime.getURL(resource))
+    for (const resource of ['lib/deepseek.js', 'lib/dom.min.js']) await import(chrome.runtime.getURL(resource))
     chrome.runtime.onMessage.addListener(
         query => dom.get.loadedElem(deepseek.selectors.chatbox).then(() => deepseek.send(query)))
 })()
