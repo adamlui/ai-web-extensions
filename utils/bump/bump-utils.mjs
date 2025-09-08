c3368fafa1399413d00121713655a027831967dd
Adam Lui <adam@kudoai.com>
2025-09-07 19:12:51 -0700

    Edited whitespace for readability

--- utils/bump/bump-utils.mjs
+++ utils/bump/bump-utils.mjs
@@ -34,7 +34,7 @@ export function bumpDateVer({ filePath, verbose = true } = {}) { // bumps YYYY.M
     const date = new Date(), today = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`
     const newVer = oldVer == today ? `${today}.1` // bump sub-ver to 1
                  : oldVer.startsWith(`${today}.`) ? `${ // bump sub-ver to 2+
-                       today}.${parseInt(oldVer.split('.').pop()) + 1}`
+                       today}.${ parseInt(oldVer.split('.').pop()) +1 }`
                  : today // bump to today
     fs.writeFileSync(filePath, fileContent.replace(new RegExp(`("?)${oldVer}("?)`), `$1${newVer}$2`), 'utf-8')
     if (verbose) this.log.success(`${nc}Updated: ${bw}v${oldVer}${nc} → ${bg}v${newVer}${nc}\n`)
