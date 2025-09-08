2ce35d5db2971ede442aae52313cd1adcf210c26
Adam Lui <adam@kudoai.com>
2025-09-07 19:11:34 -0700

    Refactored `newVer` init in `bumpDateVer()` for readability

--- utils/bump/bump-utils.mjs
+++ utils/bump/bump-utils.mjs
@@ -33,7 +33,7 @@ export function bumpDateVer({ filePath, verbose = true } = {}) { // bumps YYYY.M
     if (!oldVer) return this.log.info(`No version found in ${filePath}`)
     const date = new Date(), today = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`
     const newVer = oldVer == today ? `${today}.1` // bump sub-ver to 1
-                 : oldVer.indexOf(`${today}.`) == 0 ? `${ // bump sub-ver to 2+
+                 : oldVer.startsWith(`${today}.`) ? `${ // bump sub-ver to 2+
                        today}.${parseInt(oldVer.split('.').pop()) + 1}`
                  : today // bump to today
     fs.writeFileSync(filePath, fileContent.replace(new RegExp(`("?)${oldVer}("?)`), `$1${newVer}$2`), 'utf-8')
